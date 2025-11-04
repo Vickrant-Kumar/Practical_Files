cc922649080cae48ab171d19d0ad204563273a01
Vickrant-Kumar <vikrnt04@gmail.com>
2025-11-04 15:04:02 +0530

    Delete Java_Practical_File/EXP-7,8,9 directory

--- Java_Practical_File/EXP-7,8,9/EXP-7/WebContent/success.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-    <h2>Attendance Recorded Successfully!</h2>
-    <a href="attendance.jsp">Go Back</a>
-</body>
-</html>
--- Java_Practical_File/EXP-7,8,9/EXP-8/SpringDIApp/src/com/course.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Course {
-    private String courseName = "Spring Framework";
-
-    public String getCourseName() {
-        return courseName;
-    }
-}
